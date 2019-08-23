bcf2b1b18b730cdc24506afda7a855f3ce013547
nghiant <nghianguyen248@gmail.com>
2019-08-23 17:17:48 +0900

    add logic show null search reservation code result

--- common/js/actions/search-reservation-code.js
+++ common/js/actions/search-reservation-code.js
@@ -4,6 +4,7 @@ import {
 import api from '@lib/api';
 import generateActionCreator from '@lib/generateActionCreator';
 import { push } from 'react-router-redux';
+import i18n from '@lib/i18n';
 
 export const updateReservationCodeAction = generateActionCreator(SEARCH_RESERVATION_CODE, 'reservationCode');
 
@@ -11,8 +12,11 @@ export const searchReservationCode = (params) => {
   return dispatch => {
     return api
       .post('/api/search-reservation-code/search', { params })
-      .then(reservationCode => {
-        dispatch(updateReservationCodeAction(reservationCode));
+      .then(reservationCodeObj => {
+        if (!Object.keys(reservationCodeObj).length) {
+          reservationCodeObj.reservationCode = i18n.t('common:search_reservation_code.not_found');
+        }
+        dispatch(updateReservationCodeAction(reservationCodeObj.reservationCode));
         dispatch(push('/search_reservation_code_result'));
         // return Promise.resolve(reservationCode);
       })
