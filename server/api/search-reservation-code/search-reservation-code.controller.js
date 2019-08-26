071a4222a8ce6e930b4e76eebc386cbff2577098
nghiant <nghianguyen248@gmail.com>
2019-08-26 15:53:12 +0900

    finish call API get reservation code

--- server/api/search-reservation-code/search-reservation-code.controller.js
+++ server/api/search-reservation-code/search-reservation-code.controller.js
@@ -1,17 +1,26 @@
 import axios from 'axios';
+import moment from 'moment';
 
 export async function searchReservationCode(req, res) {
   try {
-     const options = {
-        method: "GET",
-        data: req.body.params,
-        url: `${process.env.ADMIN_PAGE_API_BASE_URL}/api/todos`,
-      };
+    const data = {};
 
-      axios(options)
-        .then((response) => {
-          res.json(response.data);
-        })
+    data.reservation_guest_first_name = req.body.params.first_name;
+    data.reservation_guest_last_name = req.body.params.last_name;
+    data.reservation_start_date = moment(req.body.params.checkin).format("YYYY-MM-DD");
+    data.reservation_end_date = moment(req.body.params.checkout).format("YYYY-MM-DD");
+    data.reservation_guest_phone_number = req.body.params.phone_number;
+
+    const options = {
+      method: "POST",
+      data,
+      url: `${process.env.ADMIN_PAGE_API_BASE_URL}/api/reservation`,
+    };
+
+    axios(options)
+      .then((response) => {
+        res.json(response.data);
+      })
   } catch(e) {
     res.status(e.status).send(e.message);
   }
